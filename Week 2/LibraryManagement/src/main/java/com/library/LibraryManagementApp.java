f50c0ccfc4a012f96950549a947fe95470883c35
Niranjan <raviniranjan428@gmail.com>
2024-08-07 10:29:43 +0530

    week 2

--- Week 2/LibraryManagement/src/main/java/com/library/LibraryManagementApp.java
+++ Week 2/LibraryManagement/src/main/java/com/library/LibraryManagementApp.java
@@ -7,9 +7,13 @@ import com.library.service.BookService;
 
 public class LibraryManagementApp {
     public static void main(String[] args) {
+        // Load the Spring context from applicationContext.xml
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        // Retrieve the BookService bean
         BookService bookService = (BookService) context.getBean("bookService");
+
+        // Call the method to display books
         bookService.displayBooks();
     }
 }
--- Week 2/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week 2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -5,6 +5,7 @@ import com.library.repository.BookRepository;
 public class BookService {
     private BookRepository bookRepository;
 
+    // Setter method for dependency injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
